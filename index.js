2671329d9593e35d09af532ec5ba44a4ac2b1cfd
nishant parashar <nnishant.parashar@gmail.com>
2023-10-19 13:21:25 +0530

    cors modified

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: 'http://localhost:3000',
+    methods:["POST", "GET"],
     credentials: true
 }));
 app.use(authRoutes);
