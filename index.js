6c3d3fc7b24148987320aec8ba8ce367a06e82a0
nishant parashar <nnishant.parashar@gmail.com>
2023-10-26 18:01:47 +0530

    updated

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ const app = express();
 db();
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+    origin:["http://localhost:3000"],
+    methods:["POST", "GET"],
+    credentials: true
+}));
 app.use(cookieParser());
 app.use(authRoutes);
 app.use(productRoutes);
