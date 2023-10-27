231380444d41fd4f0c6f16b55fc12248cb2c0763
nishant parashar <nnishant.parashar@gmail.com>
2023-10-27 16:56:36 +0530

    auth update

--- utils/authentication.js
+++ utils/authentication.js
@@ -3,11 +3,7 @@ const jwt = require("jsonwebtoken");
 exports.isAuth = async (req, res, next) => {
   
   const token = req.headers.authorization.split(" ")[1];
-  if(!token || token === ''){
-    return res.status(401).send({
-      message: "Please login to continue.."
-    });
-  }
+  
   if (token) {
     console.log("Auth token : ", token)
     let obj = await jwt.verify(token, process.env.SECRET_KEY);
