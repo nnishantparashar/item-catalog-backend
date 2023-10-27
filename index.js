063f5e946332eb30185f35c5279a9ea6de83f922
nishant parashar <nnishant.parashar@gmail.com>
2023-10-27 14:03:38 +0530

    auth

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const app = express();
 db();
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 // app.use(
 //   cors({
 //     origin: "*",
@@ -22,16 +23,16 @@ app.use(cors());
 //   })
 // );
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   next();
-// });
-app.use(cookieParser());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(authRoutes);
 app.use(productRoutes);
 app.use(cartRoutes);
